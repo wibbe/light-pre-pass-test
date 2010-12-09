af31d88f6b44e96c991176dfc31907d050aeb6d5
Daniel Wiberg <d.a.wiberg@gmail.com>
2010-12-09 01:28:17 +0100

    Lets open a window that we can render to.

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,5 +1,33 @@
 
+#include <iostream>
+
+#ifdef __APPLE__
+#include <OpenGL/gl.h>
+#include <GLUT/glut.h>
+#else
+#include <gl/gl.h>
+#include <gl/glut.h>
+#endif
+
+void render();
+
 int main(int argc, char * argv[])
 {
+   glutInit(&argc, argv);
+   glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+   glutInitWindowSize(800, 500);
+   glutCreateWindow("Light Pre-Pass Test");
+   glutDisplayFunc(&render);
+   
+   glutMainLoop();
+   
    return 0;
+}
+
+void render()
+{
+   glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+   glClear(GL_COLOR_BUFFER_BIT);
+   
+   glutSwapBuffers();
 }
